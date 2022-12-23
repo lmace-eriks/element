3df8ef51c065c825c2f4cea6d42f08e63c2e5572
Web Developer <useraccount@WebDeveloperMBP.local>
2022-12-23 11:54:40 -0600

    Adding blockStyle prop

--- react/Element.tsx
+++ react/Element.tsx
@@ -1,9 +1,11 @@
-import React, { ReactChildren, useEffect, useState } from 'react';
+import React, { ReactChildren, useEffect, useState, useRef } from 'react';
 
 interface ElementProps {
   tag: string
   attributes: Array<AttributeObject>
   children: ReactChildren
+  blockStyle: string
+  blockClass: string
   classes: Array<string>
   text: string
 }
@@ -13,18 +15,18 @@ interface AttributeObject {
   value: string
 }
 
-const Element: StorefrontFunctionComponent<ElementProps> = ({ tag, attributes, classes, text, children }) => {
-  const [openGate, setOpenGate] = useState<Boolean>(true);
+const Element: StorefrontFunctionComponent<ElementProps> = ({ blockStyle, tag, attributes, blockClass, classes, text, children }) => {
+  const openGate = useRef(true);
+  const [loading, setLoading] = useState(true);
   const [htmlAttributes, setHTMLAttributes] = useState<any>();
-  const [htmlClasses, setHTMLClasses] = useState<string>();
+  const [htmlClasses, setHTMLClasses] = useState<string>("");
+  const [styleRules, setStyleRules] = useState<any>();
 
   const defaultTag = "div";
   const CustomTag: any = !tag ? `${defaultTag}` : `${tag}`;
   const classPrefix = "eriksbikeshop-element-1-x-";
 
   const buildAttributes = () => {
-    if (!attributes) return;
-
     const tempAttributes: any = {};
 
     attributes.forEach(att => {
@@ -35,31 +37,59 @@ const Element: StorefrontFunctionComponent<ElementProps> = ({ tag, attributes, c
   }
 
   const buildClasses = () => {
-    if (!classes) {
-      setHTMLClasses("");
-      return;
-    }
+    if (!classes) return;
 
-    let tempClasses: string = "";
+    let tempClasses = "";
 
     classes.forEach((selector, index) => {
-      // Prevents a single space (" ") at the beginning of the class attribute - LM
+      // Ternary prevents a single space (" ") at the beginning of the class attribute - LM
       tempClasses = index ? `${tempClasses} ${classPrefix}${selector}` : `${classPrefix}${selector}`;
     })
 
     setHTMLClasses(tempClasses);
   }
 
+  const buildStyle = () => {
+    const styleOutput: any = {};
+    const propertiesCSS: any = [];
+    const valuesCSS: any = [];
+
+    // Split CSS rules into array - LM
+    const rules = blockStyle.split("; ");
+
+    // Remove ; from final rule in array - LM
+    rules[rules.length - 1] = rules[rules.length - 1].replace(";", "");
+
+    rules.forEach((rule) => {
+      // Split Properties and Values into separate items - LM
+      const propertiesAndValues = rule.split(": ");
+
+      // Toggle for filling Property and Value arrays - LM
+      propertiesAndValues.forEach((tempItem, index) => {
+        index % 2 === 0 ? propertiesCSS.push(tempItem) : valuesCSS.push(tempItem);
+      })
+    })
+
+    // Fill styleOutput object with properties and values - LM
+    for (let index = 0; index < rules.length; index++) {
+      styleOutput[propertiesCSS[index]] = valuesCSS[index];
+    }
+
+    setStyleRules(styleOutput);
+  }
+
   useEffect(() => {
-    if (!openGate) return;
-    setOpenGate(false);
-    buildAttributes();
-    buildClasses()
+    if (!openGate.current) return;
+    openGate.current = false;
+
+    if (blockStyle) buildStyle();
+    if (attributes) buildAttributes();
+    if (!blockClass) buildClasses();
+
+    setLoading(false);
   });
 
-  return (
-    <CustomTag {...htmlAttributes} className={htmlClasses}>{text || children}</CustomTag>
-  )
+  return !loading ? <CustomTag {...htmlAttributes} className={blockClass ? `${classPrefix}${blockClass}` : htmlClasses} style={styleRules} >{text || children}</CustomTag> : <></>;
 }
 
 Element.schema = {
@@ -75,6 +105,11 @@ Element.schema = {
     text: {
       title: "Inner Text",
       description: "Only used if there are no children / decendants.",
+      type: "string"
+    },
+    blockStyle: {
+      title: "Style",
+      description: "Accepts all Javascript CSS properties. Ex: borderRadius: 1rem; padding: 1rem;",
       type: "string"
     },
     attributes: {
